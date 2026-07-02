df06dd7b36686408ec93f17cd53f477280a2b2be
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 19:29:40 +0530

    add size constants

---
+++ lib/utils/constants/sizes.dart
@@ -0,0 +1,71 @@
+class CSizes {
+  CSizes._();
+
+  //Padding and Margin Sizes
+  static const xs = 4.0;
+  static const sm = 8.0;
+  static const md = 16.0;
+  static const lg = 24.0;
+  static const xl = 32.0;
+
+  //Icon Sizes
+  static const iconXs = 12.0;
+  static const iconSm = 16.0;
+  static const iconMd = 24.0;
+  static const iconLg = 32.0;
+
+  //Font Sizes
+  static const fontSizeSm = 14.0;
+  static const fontSizeMd = 16.0;
+  static const fontSizeLg = 18.0;
+
+  //Button Sizes
+  static const buttonHeight = 18.0;
+  static const buttonRadius = 12.0;
+  static const buttonWidth = 120.0;
+  static const buttonElevation = 4.0;
+
+  //Appbar Height
+  static const appBarHeight = 56.0;
+
+  //Image Sizes
+  static const imageThumbSize = 80.0;
+
+  //Default Spacing Between Sections
+  static const defaultSpace = 24.0;
+  static const spaceBetweenItems = 16.0;
+  static const spaceBetweenSections = 32.0;
+
+  //Border Radius
+  static const borderRadiusSm = 4.0;
+  static const borderRadiusMd = 8.0;
+  static const borderRadiusLg = 12.0;
+
+  //Divider Height
+  static const dividerHeight = 1.0;
+
+  //Product Item Dimensions
+  static const productImageSize = 120.0;
+  static const productImageRadius = 16.0;
+  static const productImageHeight = 160.0;
+
+  //Input Field
+  static const inputFieldRadius = 12.0;
+  static const spaceBetweenInputFields = 16.0;
+
+  //Card Sizes
+  static const cardRadiusLg = 16.0;
+  static const cardRadiusMd = 12.0;
+  static const cardRadiusSm = 10.0;
+  static const cardRadiusXs = 6.0;
+  static const cardElevation = 2.0;
+
+  //Image Carousel Height
+  static const imageCarouselHeight = 200.0;
+
+  //Loading Indicator Size
+  static const loadingIndicatorSize = 36.0;
+
+  //GridView Spacing
+  static const gridViewSpacing = 16.0;
+}
