87f8c5082cca27ffe71eafba022865798f076a5a
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 17:34:13 +0530

    configure theme datas and create text theme

---
+++ lib/utils/theme/app_text_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme {
+  const AppTextTheme._();
+
+  static final lightTextTheme = TextTheme();
+
+  static final darkTextTheme = TextTheme();
+}
--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -1,9 +1,18 @@
+import 'package:e_commerce/utils/theme/app_text_theme.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
   const AppTheme._();
 
-  static final lightThemeData = ThemeData();
+  static final lightThemeData = ThemeData(
+    brightness: Brightness.light,
+    fontFamily: 'Poppins',
+    textTheme: AppTextTheme.lightTextTheme,
+  );
 
-  static final darkThemeData = ThemeData();
+  static final darkThemeData = ThemeData(
+    brightness: Brightness.dark,
+    fontFamily: 'Poppins',
+    textTheme: AppTextTheme.darkTextTheme,
+  );
 }
