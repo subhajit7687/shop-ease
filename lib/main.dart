9adbb53f60d47d99bce2ae574855f437977170b9
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 17:28:26 +0530

    create and add light and dark theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,7 +10,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
+      theme: AppTheme.lightThemeData,
+      darkTheme: AppTheme.darkThemeData,
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
---
+++ lib/utils/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  const AppTheme._();
+
+  static final lightThemeData = ThemeData();
+
+  static final darkThemeData = ThemeData();
+}
