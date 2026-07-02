9e6ba00510a9a4e42104839f350a670b52fe5092
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 19:39:51 +0530

    add text strings

---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,19 @@
+class CTextStrings {
+  CTextStrings._();
+
+  //Onboarding Texts
+  static const onBoardingTitle1 = 'Choose your product';
+  static const onBoardingTitle2 = 'Select Payment Method';
+  static const onBoardingTitle3 = 'Deliver at your door step';
+
+  static const onBoardingSubtitle1 =
+      'Welcome to a World of Limitless Choices - Your Perfect Product Awaits!';
+  static const onBoardingSubtitle2 =
+      'For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!';
+  static const onBoardingSubtitle3 =
+      'From Our Doorstem to Yours - Swift, Secure, and Contactless Delivery!';
+
+  //Home
+  static const homeAppbarTitle = 'Good day for shopping';
+  static const homeAppbarSubtitle = 'Sinesta';
+}
