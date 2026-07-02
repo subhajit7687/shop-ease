8df13a57953c163ff5b8c54b47510920bdfd1873
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 17:56:02 +0530

    configure elevated button theme

--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/utils/theme/custom/app_elevated_button_theme.dart';
 import 'package:e_commerce/utils/theme/custom/app_text_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -8,11 +9,13 @@ class AppTheme {
     brightness: Brightness.light,
     fontFamily: 'Poppins',
     textTheme: AppTextTheme.lightTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.lightElevatedButtonTheme,
   );
 
   static final darkThemeData = ThemeData(
     brightness: Brightness.dark,
     fontFamily: 'Poppins',
     textTheme: AppTextTheme.darkTextTheme,
+    elevatedButtonTheme: AppElevatedButtonTheme.darkElevatedButtonTheme,
   );
 }
---
+++ lib/utils/theme/custom/app_elevated_button_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppElevatedButtonTheme {
+  const AppElevatedButtonTheme._();
+
+  static final lightElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      padding: EdgeInsets.symmetric(vertical: 18),
+      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(12),
+      ),
+    ),
+  );
+
+  static final darkElevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      elevation: 0,
+      foregroundColor: Colors.white,
+      backgroundColor: Colors.blue,
+      disabledForegroundColor: Colors.grey,
+      disabledBackgroundColor: Colors.grey,
+      padding: EdgeInsets.symmetric(vertical: 18),
+      textStyle: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadiusGeometry.circular(12),
+      ),
+    ),
+  );
+}
