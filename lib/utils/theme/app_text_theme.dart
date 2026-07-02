366741b2dcb8ab9d2ee57e2b18bf90f86bb60fad
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 17:48:09 +0530

    configure text theme

--- lib/utils/theme/app_text_theme.dart
+++ lib/utils/theme/app_text_theme.dart
@@ -3,7 +3,45 @@ import 'package:flutter/material.dart';
 class AppTextTheme {
   const AppTextTheme._();
 
-  static final lightTextTheme = TextTheme();
+  static final lightTextTheme = TextTheme(
+    //Headline
+    headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+    headlineMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+    headlineSmall: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
 
-  static final darkTextTheme = TextTheme();
+    //Title
+    titleLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+    titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+
+    //Body
+    bodyLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400),
+    bodySmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+
+    //Label
+    labelLarge: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
+    labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
+  );
+
+  static final darkTextTheme = TextTheme(
+    //Headline
+    headlineLarge: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+    headlineMedium: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w600),
+    headlineSmall: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w600),
+
+    //Title
+    titleLarge: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+    titleMedium: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w500),
+    titleSmall: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w400),
+
+    //Body
+    bodyLarge: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+    bodyMedium: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w400),
+    bodySmall: TextStyle(fontSize: 14.0, fontWeight: FontWeight.w500),
+
+    //Label
+    labelLarge: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
+    labelMedium: TextStyle(fontSize: 12.0, fontWeight: FontWeight.w400),
+  );
 }
