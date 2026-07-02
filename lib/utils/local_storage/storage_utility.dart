9a0c65b61d26513377f5dc8f9667884b17fc9754
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 21:32:34 +0530

    configure storage utility

---
+++ lib/utils/local_storage/storage_utility.dart
@@ -0,0 +1,20 @@
+import 'package:get_storage/get_storage.dart';
+
+class LocalStorage {
+  LocalStorage._internal();
+
+  static final _instance = LocalStorage._internal();
+
+  factory LocalStorage() => _instance;
+
+  final _storage = GetStorage();
+
+  Future<void> saveData<T>(String key, T value) async =>
+      await _storage.write(key, value);
+
+  T? readData<T>(String key) => _storage.read<T>(key);
+
+  void removeData(String key) async => await _storage.remove(key);
+
+  void clearAll() async => await _storage.erase();
+}
