5abd699435917065b1a91b277559cb1fedb401ba
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 18:57:47 +0530

    shift MainApp to it's own file

---
+++ lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce/utils/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: AppTheme.lightThemeData,
+      darkTheme: AppTheme.darkThemeData,
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,6 @@
-import 'package:e_commerce/utils/theme/app_theme.dart';
+import 'package:e_commerce/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  MaterialApp(
-      theme: AppTheme.lightThemeData,
-      darkTheme: AppTheme.darkThemeData,
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
-  }
 }
