6faca070624a5eeead34efe1d196a546bcc62cc7
Subhajit Roy <subhajit0061@gmail.com>
2026-07-02 18:14:13 +0530

    configure checkbox theme

--- lib/utils/theme/app_theme.dart
+++ lib/utils/theme/app_theme.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce/utils/theme/custom/bottom_sheet_theme.dart';
+import 'package:e_commerce/utils/theme/custom/checkbox_theme.dart';
 import 'package:e_commerce/utils/theme/custom/elevated_button_theme.dart';
 import 'package:e_commerce/utils/theme/custom/text_theme.dart';
 import 'package:e_commerce/utils/theme/custom/appbar_theme.dart';
@@ -14,6 +15,7 @@ class AppTheme {
     elevatedButtonTheme: CElevatedButtonTheme.lightElevatedButtonTheme,
     appBarTheme: CAppbarTheme.lightAppBarTheme,
     bottomSheetTheme: CBottomSheetTheme.lightBottomSheetTheme,
+    checkboxTheme: CCheckboxTheme.lightCheckboxTheme,
   );
 
   static final darkThemeData = ThemeData(
@@ -23,5 +25,6 @@ class AppTheme {
     elevatedButtonTheme: CElevatedButtonTheme.darkElevatedButtonTheme,
     appBarTheme: CAppbarTheme.darkAppBarTheme,
     bottomSheetTheme: CBottomSheetTheme.darkBottomSheetTheme,
+    checkboxTheme: CCheckboxTheme.darkCheckboxTheme
   );
 }
---
+++ lib/utils/theme/custom/checkbox_theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CCheckboxTheme {
+  const CCheckboxTheme._();
+
+  static final lightCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+
+  static final darkCheckboxTheme = CheckboxThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(4)),
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.white;
+      } else {
+        return Colors.black;
+      }
+    }),
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      if (states.contains(WidgetState.selected)) {
+        return Colors.blue;
+      } else {
+        return Colors.transparent;
+      }
+    }),
+  );
+}
