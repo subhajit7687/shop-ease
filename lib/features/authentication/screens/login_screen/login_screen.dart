927c0cd0ed24e0ac3b31226a45546b73c3d08cf6
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 22:24:25 +0530

    add login_screen.dart

---
+++ lib/features/authentication/screens/login_screen/login_screen.dart
@@ -0,0 +1,41 @@
+import 'package:e_commerce/common/styles/spacing_styles.dart';
+import 'package:e_commerce/features/authentication/screens/login_screen/widgets/login_divider.dart';
+import 'package:e_commerce/features/authentication/screens/login_screen/widgets/login_footer.dart';
+import 'package:e_commerce/features/authentication/screens/login_screen/widgets/login_form.dart';
+import 'package:e_commerce/features/authentication/screens/login_screen/widgets/login_logo_title.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: CSpacingStyles.paddingWithAppBarHeight,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              LoginLogoTitle(),
+              SizedBox(height: CSizes.spaceBetweenSections),
+              LoginForm(formKey: _formKey),
+              SizedBox(height: CSizes.spaceBetweenSections),
+              LoginDivider(),
+              SizedBox(height: CSizes.spaceBetweenItems),
+              LoginFooter(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
