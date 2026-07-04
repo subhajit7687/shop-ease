7aa4b05ef20bc063729d33f64f38ecedd4cd0b49
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 19:26:43 +0530

    update image_strings values

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -2,11 +2,14 @@ class CImages {
   CImages._();
 
   //App Logos
-  static const darkAppLogo = 'assets/logos/t-store-splash-logo-black.png';
-  static const lightAppLogo = 'assets/logos/t-store-splash-logo-white.png';
+  static const darkAppLogo = 'assets/logo/logo_dark.png';
+  static const lightAppLogo = 'assets/logo/logo_light.png';
 
   //Social Logos
   static const google = 'assets/logos/google-icon.png';
   static const facebook = 'assets/logos/facebook-icon.png';
+
+  static const onboardingImage1 = 'assets/images/onboarding_images/onboarding_1.png';
+  static const onboardingImage2 = 'assets/images/onboarding_images/onboarding_2.png';
+  static const onboardingImage3 = 'assets/images/onboarding_images/onboarding_3.png';
 }
- 
