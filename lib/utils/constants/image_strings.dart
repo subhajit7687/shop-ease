1297d888e502288d05dda02e17de8aab7aee056b
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 22:14:19 +0530

    update paths of google and facebook logo in image_strings.dart

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -6,10 +6,13 @@ class CImages {
   static const lightAppLogo = 'assets/logo/logo_light.png';
 
   //Social Logos
-  static const google = 'assets/logos/google-icon.png';
-  static const facebook = 'assets/logos/facebook-icon.png';
+  static const google = 'assets/logos/google.png';
+  static const facebook = 'assets/logo/facebook.png';
 
-  static const onboardingImage1 = 'assets/images/onboarding_images/onboarding_1.png';
-  static const onboardingImage2 = 'assets/images/onboarding_images/onboarding_2.png';
-  static const onboardingImage3 = 'assets/images/onboarding_images/onboarding_3.png';
+  static const onboardingImage1 =
+      'assets/images/onboarding_images/onboarding_1.png';
+  static const onboardingImage2 =
+      'assets/images/onboarding_images/onboarding_2.png';
+  static const onboardingImage3 =
+      'assets/images/onboarding_images/onboarding_3.png';
 }
