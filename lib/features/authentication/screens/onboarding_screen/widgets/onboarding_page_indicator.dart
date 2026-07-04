e0d5cb617ba57f0c462ec28abed358dfd411b5bd
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 20:05:02 +0530

    add onboarding_page_indicator.dart

---
+++ lib/features/authentication/screens/onboarding_screen/widgets/onboarding_page_indicator.dart
@@ -0,0 +1,28 @@
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:e_commerce/utils/constants/sizes.dart';
+import 'package:e_commerce/utils/device/device_utils.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnboardingPageIndicator extends StatelessWidget {
+  const OnboardingPageIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: DeviceUtils.bottomNavigationBarHeight,
+      left: CSizes.defaultSpace,
+      child: SmoothPageIndicator(
+        controller: PageController(),
+        count: 3,
+        effect: ExpandingDotsEffect(
+          dotHeight: 6,
+          activeDotColor: HelperFunctions.isDarkMode(context)
+              ? CColors.light
+              : CColors.dark,
+        ),
+      ),
+    );
+  }
+}
