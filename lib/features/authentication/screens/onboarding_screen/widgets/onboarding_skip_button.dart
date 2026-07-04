ea16503fa5b2a0a295a4a4b5bbf1f38a52179b27
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 20:44:45 +0530

    working onboarding_skip_button

--- lib/features/authentication/screens/onboarding_screen/widgets/onboarding_skip_button.dart
+++ lib/features/authentication/screens/onboarding_screen/widgets/onboarding_skip_button.dart
@@ -1,6 +1,8 @@
+import 'package:e_commerce/features/authentication/cubit/onboarding_cubit/onboarding_cubit.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/device/device_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OnboardingSkipButton extends StatelessWidget {
   const OnboardingSkipButton({super.key});
@@ -10,7 +12,12 @@ class OnboardingSkipButton extends StatelessWidget {
     return Positioned(
       top: DeviceUtils.appBarHeight,
       right: CSizes.defaultSpace,
-      child: TextButton(onPressed: () {}, child: Text('Skip')),
+      child: TextButton(
+        onPressed: () {
+          context.read<OnboardingCubit>().skipPage();
+        },
+        child: Text('Skip'),
+      ),
     );
   }
 }
