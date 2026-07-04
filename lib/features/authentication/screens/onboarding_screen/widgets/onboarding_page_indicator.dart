652ce66451f1b82c9712365f6cdb726d6cc5ae88
Subhajit Roy <subhajit0061@gmail.com>
2026-07-04 20:44:22 +0530

    working onboarding_page_indicator

--- lib/features/authentication/screens/onboarding_screen/widgets/onboarding_page_indicator.dart
+++ lib/features/authentication/screens/onboarding_screen/widgets/onboarding_page_indicator.dart
@@ -1,8 +1,10 @@
+import 'package:e_commerce/features/authentication/cubit/onboarding_cubit/onboarding_cubit.dart';
 import 'package:e_commerce/utils/constants/colors.dart';
 import 'package:e_commerce/utils/constants/sizes.dart';
 import 'package:e_commerce/utils/device/device_utils.dart';
 import 'package:e_commerce/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPageIndicator extends StatelessWidget {
@@ -14,7 +16,8 @@ class OnboardingPageIndicator extends StatelessWidget {
       bottom: DeviceUtils.bottomNavigationBarHeight,
       left: CSizes.defaultSpace,
       child: SmoothPageIndicator(
-        controller: PageController(),
+        controller: context.read<OnboardingCubit>().pageController,
+        onDotClicked: context.read<OnboardingCubit>().dotNavigationClick,
         count: 3,
         effect: ExpandingDotsEffect(
           dotHeight: 6,
