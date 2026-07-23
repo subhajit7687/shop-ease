5bf18eec9db96c043efe4c9991fecb9d05639d14
Subhajit Roy <subhajit0061@gmail.com>
2026-07-23 12:57:56 +0530

    Configure firebase console

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:e_commerce/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MainApp());
 }
